6e57a7aabd5faa61aa3ed562cf240f0fd448685f
Sultan <ayanh85@gmail.com>
2024-11-14 01:11:16 +0530

    crud started

---
+++ src/app/Model/Employee.ts
@@ -0,0 +1,21 @@
+export class Employee{
+    empid : number;
+    empname : string;
+    empemail : string;
+    empmobile : string;
+    emppassword : string;
+    empcity: string;
+    empstatus : boolean
+
+    constructor()
+    {
+        this.empid = 1;
+        this.empname = "";
+        this.empemail = "";
+        this.empmobile = "";
+        this.emppassword = "";
+        this.empcity = "";
+        this.empstatus = false;
+    }
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,69 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Employee } from './Model/Employee';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Angular_LocalStorageCRUD';
+  
+  employeeobj : Employee = new Employee();
+  employeeform : FormGroup = new FormGroup({});
+  employeeList : Employee[] = [];
+
+
+constructor()
+{
+  this.createform();
+
+  const myempdata = localStorage.getItem("empdata");
+  if(myempdata != null)
+  {
+    const empdata = JSON.parse(myempdata);
+    this.employeeList = empdata;
+  }
+}
+
+  createform()
+  {
+    this.employeeform = new FormGroup({
+      empid : new FormControl(this.employeeobj.empid),
+      empname : new FormControl(this.employeeobj.empname),
+      empemail : new FormControl(this.employeeobj.empemail),
+      empmobile : new FormControl(this.employeeobj.empmobile),
+      emppassword : new FormControl(this.employeeobj.emppassword),
+      empcity : new FormControl(this.employeeobj.empcity),
+      empstatus : new FormControl(this.employeeobj.empstatus)
+    })
+  }
+
+  reset()
+  {
+    this.employeeobj = new Employee();
+    this.createform();
+  }
+
+
+  SaveData()
+  {
+    const empolddata = localStorage.getItem("empdata");
+    if(empolddata!= null)
+    {
+      const parseData = JSON.parse(empolddata);
+      this.employeeform.controls['empid'].setValue(parseData.length + 1);
+      this.employeeList.unshift(this.employeeform.value);
+    }
+    else{
+      this.employeeList.unshift(this.employeeform.value);
+    }
+    localStorage.setItem("empdata",JSON.stringify(this.employeeList));
+    this.reset();
+  }
+
+  
 }
